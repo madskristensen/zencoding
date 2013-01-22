954252314de77662c344d991623b9bedd9c7bf41
Mads Kristensen <post@madskristensen.net>
2013-01-21 20:07:15 -0800

    First commit

---
+++ ZenCoding.Test/Grouping.cs
@@ -0,0 +1,34 @@
+﻿
+//using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System.Collections.Generic;
+//using System.Text;
+//using P = ZenCoding.Parser;
+
+//namespace ZenCoding.Test
+//{
+//    [TestClass]
+//    public class Grouping
+//    {
+//        private ZenCoding.Parser _parser;
+
+//        [TestInitialize]
+//        public void Initialize()
+//        {
+//            _parser = new ZenCoding.Parser();
+//        }
+
+//        [TestMethod]
+//        public void GroupingSimple()
+//        {
+//            string result = _parser.Parse("div>(header>h1)+footer", ZenType.HTML);
+//            string expected = "<div></div>" +
+//                                "<div>" +
+//                                  "<p><span></span><em></em></p>" +
+//                                  "<blockquote></blockquote>" +
+//                                "</div>";
+
+//            Assert.AreEqual(expected, result);
+//        }
+//    }
+//}
---
+++ ZenCoding/Parser.cs
@@ -0,0 +1,27 @@
+﻿
+namespace ZenCoding
+{
+    public class Parser
+    {
+        public string Parse(string zenSyntax, ZenType type)
+        {
+            switch (type)
+            {
+                case ZenType.CSS:
+                    break;
+
+                case ZenType.HTML:
+                    HtmlParser parser = new HtmlParser();
+                    return parser.Parse(zenSyntax.Trim());
+            }
+
+            return null;
+        }
+    }
+
+    public enum ZenType
+    {
+        CSS,
+        HTML
+    }
+}
