3e372984a61346d7ad0d7c091a51065ae9028494
Adeel <adeelbm@outlook.com>
2013-10-29 18:26:09 +0300

    Cleared commented code

--- ZenCoding/Html/CustomHtmlInput.cs
+++ ZenCoding/Html/CustomHtmlInput.cs
@@ -14,7 +14,7 @@ namespace ZenCoding
         protected override void Render(System.Web.UI.HtmlTextWriter writer)
         {
             writer.Write(Environment.NewLine);
-            //this will output the start of the img element - <img
+
             writer.WriteBeginTag("input");
 
             writer.WriteAttribute("type", Attributes["type"]);
@@ -26,10 +26,6 @@ namespace ZenCoding
                 if (attr != "name" && attr != "type")
                     writer.WriteAttribute(attr, Attributes[attr]);
             }
-
-            //this will output the src and alt attributes
-            //writer.WriteAttribute("src", src);
-            //writer.WriteAttribute("alt", alt);
 
             writer.Write(HtmlTextWriter.SelfClosingTagEnd);
             writer.Write(Environment.NewLine);
