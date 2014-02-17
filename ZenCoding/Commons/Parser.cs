694e709f45690224ab9e1a2aa4ca75e80c741f67
Adeel <adeelbm@outlook.com>
2014-02-17 08:35:46 +0300

    Code: Improved file structure.

---
+++ ZenCoding/Commons/Parser.cs
@@ -0,0 +1,31 @@
+﻿namespace ZenCoding
+{
+    public class Parser
+    {
+        IZenParser _parser;
+
+        public string Parse(string zenSyntax, ZenType type)
+        {
+            if (zenSyntax == null)
+                return null;
+
+            switch (type)
+            {
+                case ZenType.HTML:
+                    this._parser = new HtmlParser();
+                    return this._parser.Parse(zenSyntax.Trim());
+                case ZenType.CSS:
+                    this._parser = new CssParser();
+                    return this._parser.Parse(zenSyntax.Trim());
+            }
+
+            return null;
+        }
+    }
+
+    public enum ZenType
+    {
+        HTML,
+        CSS
+    }
+}
